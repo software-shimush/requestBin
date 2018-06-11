3b2c73641905e42a6e7fb8d9ade6d52b4c61adb5
vagrant <vagrant@homestead>
2018-06-11 16:26:35 +0000

    starting front-end

--- app/User.php
+++ app/User.php
@@ -4,7 +4,6 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
 class User extends Authenticatable
 {
     use Notifiable;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,9 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/createBin') }}">{{ __('Create Bin') }}</a>
+                            </li>
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
