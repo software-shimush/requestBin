ae3ea9209ee5b9b74f152fda5f362b1cfb3a6e51
vagrant <vagrant@homestead>
2018-06-13 19:13:09 +0000

    correcting

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/{id}', 'StoreRequestsController@update');
+Route::get('/{id}', 'StoreRequestControllers@update');
 
