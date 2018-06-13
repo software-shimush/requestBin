3a1e27fcb52dd4f08541a0c6385e0e155928c739
vagrant <vagrant@homestead>
2018-06-13 17:59:51 +0000

    wendsUpdate

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::all('/{id}', 'StoreRequestController@update');
+Route::any('/{id}', 'StoreRequestsController@update');
 
