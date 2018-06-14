a6de46648aeea67b45f3d183be8b5f9ecd58a6c4
mjscode <go341lst@gmail.com>
2018-06-14 11:15:22 -0400

    aofio

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< HEAD
-Route::get('/{id}', 'StoreRequestsController@update');
-=======
 
 Route::any('/{id}', 'StoreRequestsController@update');
 
->>>>>>> 8d0e745ebbf233efa3f376d53b819db9c6cf2599
 
--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,13 @@ Route::get('/', function () {
 Route::get('createBin', function () {
     return view('createBin');
 });
-<<<<<<< HEAD
-// Route::post('urlGenerater', function () {
-//     return 'generated';
-// });
-=======
 //Route::post('urlGenerater', function () {
    // return 'generated';
 //});
->>>>>>> 8d0e745ebbf233efa3f376d53b819db9c6cf2599
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 //Route::get('/url', 'HomeController@makeURL');
 
-<<<<<<< HEAD
-Route::post('/url','Url_generatorController@url' );
-=======
 Route::post('/urlGenerater','Url_generatorController@url' );
->>>>>>> 8d0e745ebbf233efa3f376d53b819db9c6cf2599
