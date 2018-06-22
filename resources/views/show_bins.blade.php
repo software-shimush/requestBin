c2c57a6a73a3be7932c6692ce804cb686cf6f078
vagrant <vagrant@homestead>
2018-06-22 14:45:11 +0000

    a

--- resources/views/show_bins.blade.php
+++ resources/views/show_bins.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h1> You're Bins </h1>
+            <h1> Your Bins </h1>
         </div>
         </div>
         <div class="row justify-content-center">
